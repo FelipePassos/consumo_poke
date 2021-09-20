905fd6c52eab828dd80d53b03af25cf5749f8cd3
J. Felipe Passos <jfelipepassos@gmail.com>
2021-09-19 23:47:54 -0300

    first

---
+++ api.js
@@ -0,0 +1,10 @@
+const request = require('request'); 
+
+const hostname = 'https://pokeapi.co/api/v2';
+const path = '/pokemon/1/';
+
+request(`${hostname}${path}`, (err, res, body) => {
+
+    console.log(body);
+
+})
